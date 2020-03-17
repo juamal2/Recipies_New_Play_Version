242f099d72cd37f3e42c030450ad8fbcac8fb71f
juamal <juamal27@gmail.com>
2020-03-17 12:08:57 +0000

    inital project setup

---
+++ app/models/Note.scala
@@ -0,0 +1,12 @@
+package models
+
+import play.api.libs.json.OFormat
+
+case class Note (subject: String, message :String){
+
+}
+
+object JsonFormats {
+  import play.api.libs.json.Json
+  implicit val feedFormat: OFormat[Note] = Json.format[Note]
+}
